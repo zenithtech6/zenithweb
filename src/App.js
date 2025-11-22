b768f24cf07a073202868c7dd04fbee35b5db7ba
Shubham <shubhamwanar@gmail.com>
2025-11-22 16:31:33 +0530

    pipeline test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello<code>tzenith tech</code>: your test pipeline is working
         </p>
         <a
           className="App-link"
